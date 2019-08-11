2f2c9661e9e0e5cc58d0c729c96e9412af0c7a99
downrightmike <downrightmike@gmail.com>
2019-08-10 20:44:01 -0700

    event handling

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,48 @@
 <template>
   <div id="app">
-    <app-header title="header title">
-      <p>Some text</p>
-    </app-header>
-    <app-players v-bind:players="players"/>
-    <local-component/>
+    <!--Event Handling-->
+    <app-counter></app-counter>
+    <app-counter></app-counter>
+    <!--Each appcounter has it's own state so these will be indepentdent-->
+    <app-counter></app-counter>
+    <app-counter></app-counter>
+
+    <!-- use v-on to monitor dom for click and then update the value-->
+    <app-counter2></app-counter2>
+    <input type="text" v-on:keyup="number++">
+    <!-- counts the number of vharacters entered-->
+    {{ number}}
   </div>
 </template>
 
 <script>
 import Vue from "vue";
 
-Vue.component("app-header", {
-  props: ["title"],
-  template: "<div>{{title}}: <slot></slot></div>"
-});
-
-Vue.component("app-players", {
-  props: ["players"],
-  template:
-    "<div><ul><li v-for='player in players'><app-player v-bind:name='player.name'/></li></ul></div>"
-});
-
-Vue.component("app-player", {
-  props: ["name"],
-  template: "<div>{{name}}</div>"
-});
-var localComponent = {
-  template: "<div>local component</div>"
-};
-
 export default {
   name: "App",
   data: function() {
     return {
-      title: "some title",
-      players: [{ name: "Bart" }, { name: "Joe" }]
+      number: 0
     };
-  },
-  components: {
-    "local-component": localComponent
-  },
-  //Life cycle hooks
-  created: function() {
-    console.log("created state");
-  },
-  mounted: function() {
-    console.log("mounted state");
   }
 };
+
+Vue.component("app-counter", {
+  data: function() {
+    return {
+      value: 0
+    };
+  },
+  template: '<button v-on:click="value++">{{ value }} </button>'
+});
+Vue.component("app-counter2", {
+  data: function() {
+    return {
+      value: 0
+    };
+  },
+  template: '<button v-on:mouseover="value++">{{ value }} </button>'
+});
 </script>
 
 <style>
