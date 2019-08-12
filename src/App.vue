242b9982ca0c6c26179ba4963a7914a69bd8dec3
downrightmike <downrightmike@gmail.com>
2019-08-11 22:08:29 -0700

    event&key modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <!--Event Handling-->
-    <app-counter></app-counter>
+    <app-counter initialValue="2"></app-counter>
     <app-counter></app-counter>
     <!--Each appcounter has it's own state so these will be indepentdent-->
     <app-counter></app-counter>
-    <app-counter></app-counter>
-
     <!-- use v-on to monitor dom for click and then update the value-->
     <app-counter2></app-counter2>
     <input type="text" v-on:keyup="number++">
     <!-- counts the number of vharacters entered-->
     {{ number}}
+    <!--Event modifier-->
+    <form v-on:submit.prevent="number++">
+      <input type="text">
+    </form>
+    <!-- Key modifier-->
+    <input type="text" v-on:keyup.enter="number++">
   </div>
 </template>
 
@@ -28,12 +32,19 @@ export default {
 };
 
 Vue.component("app-counter", {
+  props: ["initialValue"],
   data: function() {
     return {
       value: 0
     };
   },
-  template: '<button v-on:click="value++">{{ value }} </button>'
+  methods: {
+    count: function(initialValue) {
+      this.value += parseInt(initialValue);
+      console.log(this.value);
+    }
+  },
+  template: '<button v-on:click.once="count">{{ value }} </button>'
 });
 Vue.component("app-counter2", {
   data: function() {
